fd27590b868cb6ce2fe5503b3e26b287b17501db
quocnguyencong <hoanglantim542000@gmail.com>
2024-08-08 14:08:22 +0700

    Update script

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://myawsbucket-25251325.s3-website-us-east-1.amazonaws.com/api/v0",
 };
 
 /*
